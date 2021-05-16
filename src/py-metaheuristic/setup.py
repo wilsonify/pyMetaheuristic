ec20c44c6f885291800efa6325fcd75f81f6a6ac
wilsonify <tom.a.wilson@gmail.com>
2021-05-16 10:39:27 -0400

    create python package

---
+++ src/py-metaheuristic/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pyMetaheuristic',
+    version='',
+    packages=['pyMetaheuristic'],
+    url='',
+    license='',
+    author='',
+    author_email='',
+    description=''
+)
