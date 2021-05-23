556e27a144363c39afb5a7348e15e7e642a59f0e
Tom Wilson <tom.andrew.wilson@gmail.com>
2021-05-23 10:31:11 -0400

    resolve lint conventions

--- src/py-metaheuristic/pyMetaheuristic/dispersive_flies_optimization/dfo.py
+++ src/py-metaheuristic/pyMetaheuristic/dispersive_flies_optimization/dfo.py
@@ -1,3 +1,24 @@
+"""
+Dispersive flies optimisation (DFO) is a bare-bones swarm intelligence algorithm which is
+inspired by the swarming behaviour of flies hovering over food sources.
+
+DFO is a simple optimiser which works by iteratively trying to improve a candidate solution
+with regard to a numerical measure that is calculated by a fitness function.
+
+Each member of the population, a fly or an agent, holds a candidate solution whose suitability
+can be evaluated by their fitness value.
+
+Optimisation problems are often formulated as either minimisation or maximisation problems.
+
+DFO [2] was introduced with the intention of analysing a simplified swarm intelligence algorithm
+with the fewest tunable parameters and components.
+
+In the first work on DFO, this algorithm was compared against a few other existing swarm intelligence
+techniques using error, efficiency and diversity measures.
+It is shown that despite the simplicity of the algorithm, which only uses agents’ position vectors at time t
+to generate the position vectors for time t + 1, it exhibits a competitive performance.
+
+"""
 ############################################################################
 
 # Created by: Prof. Valdecy Pereira, D.Sc.
@@ -19,36 +40,24 @@ import os
 import numpy as np
 
 
-# Function: Initialize Variables
-def initial_flies(
-        swarm_size=3,
-        min_values=(-5, -5),
-        max_values=(5, 5),
-        target_function=target_function,
-):
+def initial_flies(target_function, swarm_size=3, min_values=(-5, -5), max_values=(5, 5)):
+    """Initialize Variables"""
     position = np.zeros((swarm_size, len(min_values) + 1))
     for i in range(swarm_size):
-        for j in range(len(min_values)):
-            r = int.from_bytes(os.urandom(8), byteorder="big") / ((1 << 64) - 1)
-            position[i, j] = min_values[j] + r * (max_values[j] - min_values[j])
+        for j, _ in enumerate(min_values):
+            random_int = int.from_bytes(os.urandom(8), byteorder="big") / ((1 << 64) - 1)
+            position[i, j] = min_values[j] + random_int * (max_values[j] - min_values[j])
         position[i, -1] = target_function(position[i, 0: position.shape[1] - 1])
     return position
 
 
-# Function: Update Position
-def update_position(
-        position,
-        neighbour_best,
-        swarm_best,
-        min_values=(-5, -5),
-        max_values=(5, 5),
-        fly=0,
-        target_function=target_function,
-):
+def update_position(target_function, position, neighbour_best, swarm_best, min_values=(-5, -5), max_values=(5, 5),
+                    fly=0):
+    """Update Position"""
     for j in range(position.shape[1] - 1):
-        r = int.from_bytes(os.urandom(8), byteorder="big") / ((1 << 64) - 1)
+        random_int = int.from_bytes(os.urandom(8), byteorder="big") / ((1 << 64) - 1)
         position[fly, j] = np.clip(
-            (neighbour_best[j] + r * (swarm_best[j] - position[fly, j])),
+            (neighbour_best[j] + random_int * (swarm_best[j] - position[fly, j])),
             min_values[j],
             max_values[j],
         )
@@ -57,12 +66,12 @@ def update_position(
 
 
 def dispersive_fly_optimization(
+        target_function,
         swarm_size=3,
         min_values=(-5, -5),
         max_values=(5, 5),
         generations=50,
-        dt=0.2,
-        target_function=target_function,
+        thresh=0.2
 ):
     """
     # DFO Function
@@ -73,36 +82,32 @@ def dispersive_fly_optimization(
     :param min_values:
     :param max_values:
     :param generations:
-    :param dt:
+    :param thresh:
     :param target_function:
     :return:
     """
     count = 0
-    population = initial_flies(
-        swarm_size=swarm_size,
-        min_values=min_values,
-        max_values=max_values,
-        target_function=target_function,
-    )
+    population = initial_flies(target_function=target_function, swarm_size=swarm_size, min_values=min_values,
+                               max_values=max_values)
     neighbour_best = np.copy(population[population[:, -1].argsort()][0, :])
     swarm_best = np.copy(population[population[:, -1].argsort()][0, :])
     while count <= generations:
         print("Generation: ", count, " of ", generations, " f(x) = ", swarm_best[-1])
         for i in range(swarm_size):
             population = update_position(
-                population,
-                neighbour_best,
-                swarm_best,
+                target_function=target_function,
+                position=population,
+                neighbour_best=neighbour_best,
+                swarm_best=swarm_best,
                 min_values=min_values,
                 max_values=max_values,
-                fly=i,
-                target_function=target_function,
+                fly=i
             )
-            r = int.from_bytes(os.urandom(8), byteorder="big") / ((1 << 64) - 1)
-            if r < dt:
-                for j in range(len(min_values)):
-                    r = int.from_bytes(os.urandom(8), byteorder="big") / ((1 << 64) - 1)
-                    population[i, j] = min_values[j] + r * (
+            random_number = int.from_bytes(os.urandom(8), byteorder="big") / ((1 << 64) - 1)
+            if random_number < thresh:
+                for j, _ in enumerate(min_values):
+                    random_number = int.from_bytes(os.urandom(8), byteorder="big") / ((1 << 64) - 1)
+                    population[i, j] = min_values[j] + random_number * (
                             max_values[j] - min_values[j]
                     )
                 population[i, -1] = target_function(
