1b580ea3da1dcde9c39a07698ebd5311eda37463
wilsonify <tom.a.wilson@gmail.com>
2021-06-19 19:41:06 -0400

    simplify dfo test

--- tests/test_metaheuristic/test_dispersive_flies_optimization/test_dispersive_fly_optimization.py
+++ tests/test_metaheuristic/test_dispersive_flies_optimization/test_dispersive_fly_optimization.py
@@ -1,14 +1,12 @@
 import math
 from pprint import pprint
 
-import numpy as np
 import pyMetaheuristic
 import pytest
-from matplotlib import pyplot as plt
 from pyMetaheuristic import dispersive_flies_optimization
 from pyMetaheuristic.dispersive_flies_optimization import dfo
 from pyMetaheuristic.objectives import easom
-import os
+
 
 def test_smoke():
     """is anything on fire"""
@@ -26,106 +24,19 @@ def test_dispersive_flies_optimization(front):
     :return:
     """
 
-    # Target Function - Values
-    front_1 = front[:, 0]
-    front_2 = front[:, 1]
-    func_1_values = front[:, -1]
-
-    # Target Function - Plot
-    plt.style.use("bmh")
-    fig = plt.figure(figsize=(15, 15))
-    ax = fig.add_subplot(111, projection="3d")
-    ax.set_xlabel("$x_1$", fontsize=25, labelpad=20)
-    ax.set_ylabel("$x_2$", fontsize=25, labelpad=20)
-    ax.set_zlabel("$f(x_1, x_2)$", fontsize=25, labelpad=20)
-    ax.scatter(front_1, front_2, func_1_values, c=func_1_values, s=50, alpha=0.3)
-    ax.scatter(
-        math.pi, math.pi, -1, c="red", s=100, alpha=1, edgecolors="k", marker="o"
+    dfo_search = dfo.dispersive_fly_optimization(
+        target_function=easom,
+        swarm_size=50,
+        min_values=[-5, -5],
+        max_values=[5, 5],
+        generations=100,
+        thresh=0.2
     )
-    ax.text(
-        math.pi - 1.0,
-        math.pi - 1.5,
-        -1,
-        "$x_1 = $" + str(round(math.pi, 2)) + " ; $x_2 = $" + str(round(math.pi, 2)),
-        size=15,
-        zorder=1,
-        color="k",
-    )
-    ax.text(
-        math.pi + 0.5,
-        math.pi - 2.5,
-        -1,
-        "$f(x_1;x_2) = $" + str(-1),
-        size=15,
-        zorder=1,
-        color="k",
-    )
-    plt.savefig(f"{os.path.basename(__file__)}.png")
 
-    # DFO - Parameters
-    ss = 50
-    minv = [-5, -5]
-    maxv = [5, 5]
-    iterations = 100
-    dt = 0.2
-    tgt = easom
-
-    # DFO - Algorithm
-    dfo_search = dfo.dispersive_fly_optimization(target_function=tgt, swarm_size=ss, min_values=minv, max_values=maxv,
-                                                 generations=iterations, thresh=dt)
-
-    # DFO - Solution
     variables = dfo_search[:-1]
     minimum = dfo_search[-1]
-    print(
-        "Variables: ",
-        np.around(variables, 4),
-        " Minimum Value Found: ",
-        round(minimum, 4),
-    )
     assert minimum == pytest.approx(-1.0, abs=0.0001)
     assert list(variables) == [
         pytest.approx(math.pi, abs=0.001),
         pytest.approx(math.pi, abs=0.001),
     ]
-
-    # DFO - Plot Solution
-    plt.style.use("bmh")
-    fig = plt.figure(figsize=(15, 15))
-    ax = fig.add_subplot(111, projection="3d")
-    ax.set_xlabel("$x_1$", fontsize=25, labelpad=20)
-    ax.set_ylabel("$x_2$", fontsize=25, labelpad=20)
-    ax.set_zlabel("$f(x_1, x_2)$", fontsize=25, labelpad=20)
-    ax.scatter(front_1, front_2, func_1_values, c=func_1_values, s=50, alpha=0.3)
-    ax.scatter(
-        variables[0],
-        variables[1],
-        minimum,
-        c="b",
-        s=150,
-        alpha=1,
-        edgecolors="k",
-        marker="s",
-    )
-    ax.text(
-        math.pi - 1.0,
-        math.pi - 1.5,
-        -1,
-        "$x_1 = $"
-        + str(round(variables[0], 2))
-        + " ; $x_2 = $"
-        + str(round(variables[1], 2)),
-        size=15,
-        zorder=1,
-        color="k",
-    )
-    ax.text(
-        math.pi + 0.5,
-        math.pi - 2.5,
-        -1,
-        "$f(x_1;x_2) = $" + str(round(minimum, 4)),
-        size=15,
-        zorder=1,
-        color="k",
-    )
-    plt.savefig(f"{os.path.basename(__file__)}.png")
